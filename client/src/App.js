7a3d1e1696938e170c8a85370d2aa6c0a58c1118
OriSass <67857592+OriSass@users.noreply.github.com>
2020-09-22 00:31:47 +0300

    404

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Homepage from './components/Homepage';
 import Playlist from './components/Playlist';
 import Album from './components/Album';
 import Artist from './components/Artist';
 import Song from './components/Song';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
     
@@ -19,11 +20,7 @@ function App() {
         <Route path="/album/:id" exact component={Album}/>
         <Route path="/artist/:id" exact component={Artist}/>
         <Route path="/song/:id" exact component={Song}/>
-        <Route render={({match, location, history}) => (
-          <div key='404'>
-              ERROR 404, PAGE NOT FOUND!??!
-          </div>
-        )} />
+        <Route path="*" component={NotFoundPage} />
       </Switch>
     </Router>
     </div>
---
+++ client/src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    
+  return (
+    <div className="App">
+    <br></br><br></br>
+    404 page not found
+    </div>
+  );
+}
+
+export default App;

42e816ef66c14213a5e1d249b74374ec68aeeda4
OriSass <ori.sass1@gmail.com>
2021-01-28 13:35:32 +0200

    remarked analytics manager

--- client/src/Analytics/AnalyticsManager.js
+++ client/src/Analytics/AnalyticsManager.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-var mixpanel = require('mixpanel-browser');
+// require('dotenv').config();
+// var mixpanel = require('mixpanel-browser');
 
-mixpanel.init(process.env.MIXPANEL_ID);
+// mixpanel.init(process.env.MIXPANEL_ID);
 
-const actions = { track: (eventName, props) => {
-    //mixpanel.track(eventName, props);
-}}
-export let Mixpanel = actions;
+// const actions = { track: (eventName, props) => {
+//     //mixpanel.track(eventName, props);
+// }}
+// export let Mixpanel = actions;
